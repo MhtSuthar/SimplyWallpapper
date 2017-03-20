2b4e99ddef06e0677d841684a2e372300992ddf6
Mohit Suthar <mht.str@gmail.com>
2017-03-20 11:18:33 +0530

    Ads

--- app/src/main/java/com/wall/dashboard/HomeActivity.java
+++ app/src/main/java/com/wall/dashboard/HomeActivity.java
@@ -10,6 +10,9 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.WindowManager;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.wall.R;
 import com.wall.base.BaseActivity;
 import com.wall.dashboard.fragment.CategoryFragment;
@@ -23,6 +26,9 @@ public class HomeActivity extends BaseActivity {
     public WallListFragment mWallListFragment;
     public CategoryFragment mCategoryFragment;
     private SharedPreferences sharedPreferences;
+    private AdView mAdView;
+    private InterstitialAd mInterstitialAd;
+    private boolean mLoadFullScreenAds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,23 @@ public class HomeActivity extends BaseActivity {
 
     private void init() {
         mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mAdView = (AdView) findViewById(R.id.adView);
+
+        /**
+         * Footer Ads
+         */
+        AdRequest adRequest = new AdRequest.Builder()
+                .build();
+        mAdView.loadAd(adRequest);
+
+        /**
+         * FullScreen Ads
+         */
+        mInterstitialAd = new InterstitialAd(this);
+        // set the ad unit ID
+        mInterstitialAd.setAdUnitId(getString(R.string.interstitial_full_screen));
+        // Load ads into Interstitial Ads
+        mInterstitialAd.loadAd(adRequest);
     }
 
     private void fullScreen() {
@@ -71,5 +94,38 @@ public class HomeActivity extends BaseActivity {
         mViewPager.setAdapter(adapter);
         mViewPager.setCurrentItem(0);
         mViewPager.setOffscreenPageLimit(2);
+    }
+
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mInterstitialAd.isLoaded() && !mLoadFullScreenAds){
+            mLoadFullScreenAds = true;
+            mInterstitialAd.show();
+        }else
+            super.onBackPressed();
     }
 }
